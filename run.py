4018f7807905c477850d215259af51bbd7f9ae56
nabasnoogo <nabasnak7@gmail.com>
2025-07-10 12:46:41 +0000

    Synchronisation avec code local mis à jou

--- run.py
+++ run.py
@@ -1,13 +1,8 @@
-# run.py
-
-# On importe la fonction create_app depuis le package app
 from app import create_app
+from waitress import serve
 
-# On crée une instance de l'application Flask en appelant la fonction create_app()
 app = create_app()
 
-# Ce bloc s'exécute uniquement si le fichier est exécuté directement
 if __name__ == '__main__':
-    # On démarre l'application Flask en mode debug
-    # Cela permet de voir les erreurs facilement et de recharger automatiquement quand on modifie le code
-    app.run(debug=True)
+    # Démarre l'app en mode production avec waitress
+    serve(app, host='0.0.0.0', port=8000)
